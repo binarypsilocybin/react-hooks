0d9b00d8fb6b76c0576c8d8605b4ebf6708a6882
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-11 19:46:58 +0100

    2 files modified

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,1 +1,11 @@
-import React,
+import React,{ useState} from "react";
+import ReactDOM from "react-dom";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating(){
+
+  function Start(){
+    return <FaStar />
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import Input from './components/checkBox.js';
 import App from './App.js';
+import StarRating from './components/StarRating.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Input />
+    <StarRating />
   </React.StrictMode>,
   document.getElementById('root')
 );
