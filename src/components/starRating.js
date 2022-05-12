3c39b90fa30d11a7606711c86e71e77f70a8e213
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-12 09:37:03 +0100

    1 file modified

--- src/components/starRating.js
+++ src/components/starRating.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-export default function StarRatings({ totalStars }) {
+export default function StarRatings({ totalStars = 5 }) {
   const createArray = (length) => [...Array(length)];
-  function Star() {
-    return <FontAwesomeIcon icon={faStar} />;
+  function Star({ selected = false }) {
+    return <FontAwesomeIcon icon={faStar} color={selected ? 'red' : 'gray'} />;
   }
 
   return createArray(totalStars).map((n, i) => <Star key={i} />);
